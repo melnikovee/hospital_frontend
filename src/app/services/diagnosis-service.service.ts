4a08dbb76a7b15553fead29be71be8b0ffda782c
nesterov <nesterov@email@mail.ru>
2019-07-20 10:34:55 +0300

    добавил сервис диагнозов

---
+++ src/app/services/diagnosis-service.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Doctor} from '../models/doctor';
+import {Specialty} from '../models/specialty';
+import {Diagnosis} from "../models/diagnosis";
+
+@Injectable()
+export class DiagnosisService {
+
+  private diagnosisUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.diagnosisUrl = 'http://localhost:8080/diagnoses';
+  }
+
+  public findAll(): Observable<Diagnosis[]> {
+    return this.http.get<Diagnosis[]>(this.diagnosisUrl, {headers: new HttpHeaders().set('Content-Type', 'application/json')});
+  }
+
+  public save(diagnosis: Diagnosis) {
+    return this.http.post<Diagnosis>(this.diagnosisUrl, diagnosis, {headers: new HttpHeaders().set('Content-Type', 'application/json')});
+  }
+
+  deleteDiagnosis(id: number): Observable<Object> {
+    return this.http.delete(`${this.diagnosisUrl}/${id}`, {headers: new HttpHeaders().set('Content-Type', 'application/json')});
+  }
+}
