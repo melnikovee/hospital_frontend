1cdd0326bdd81f2d77956c09459135e1805b0872
nesterov <nesterov@email@mail.ru>
2019-08-03 17:02:30 +0300

    lint

--- src/app/features/lists/special-shift-list/special-shift-list.component.ts
+++ src/app/features/lists/special-shift-list/special-shift-list.component.ts
@@ -1,13 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialog, MatTableDataSource} from '@angular/material';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Composite} from '../../_models/composite';
-import {CompositeService} from '../../_services/composite-service.service';
 import {SpecialShift} from '../../_models/special-shift';
-import {PatientSpecialShift} from '../../_models/patient-special-shift';
 import {SpecialShiftService} from '../../_services/special-shift-service.service';
-import {PatientSpecialShiftService} from '../../_services/patient-special-shift-service.service';
-import {DialogDiagnosisFormComponent} from '../../forms/dialog-diagnosis-form/dialog-diagnosis-form.component';
+// tslint:disable-next-line:max-line-length
 import {SpecialShiftUpdateDialogFormComponent} from '../../forms/special-shift-update-dialog-form/special-shift-update-dialog-form.component';
 
 
@@ -49,7 +45,6 @@ export class SpecialShiftListComponent implements OnInit {
   }
 
   checkDate(specialShift: SpecialShift) {
-    //console.log(Date.parse(specialShift.date + ' ' + specialShift.endTime));
     return Date.parse(specialShift.date + ' ' + specialShift.endTime) > Date.now();
   }
 
